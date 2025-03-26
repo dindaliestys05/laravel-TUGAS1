806de31b555595bbbc87ae3b392941c0939c1393
smurfpink <makaroni3155@gmail.com>
2025-03-26 15:33:25 +0700

    [Pertemuan 3] Membuat Layout dan Component

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function(){
+    $title = "Homepage";
+   
+    return view('web.homepage',['title'=>$title]);
+   });
+   Route::get('Produk', function(){
+    $title = "Produk";
+    return view('web.Produk',['title'=>$title]);
+   });
+   Route::get('product/{slug}', function($slug){
+    $title = "Single Product";
+    return view('web.single_product',['title'=>$title,'slug'=>$slug]);
+   });
+   Route::get('categories', function(){
+    $title = "Categories";
+    return view('web.categories',['title'=>$title]);
+   });
+   Route::get('category/{slug}', function($slug){
+    $title = "Single Category";
+    return view('web.single_category',['title'=>$title,'slug'=>$slug]);
+   });
+   Route::get('cart', function(){
+    $title = "Cart";
+    return view('web.cart',['title'=>$title]);
+   });
+   Route::get('checkout', function(){
+    $title = "Checkout";
+    return view('web.checkout',['title'=>$title]);
+   });
+
+
 
 // Inisialisasi array untuk menyimpan pesanan sementara
 $orders = [];
 
 // Route untuk halaman utama
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
 
